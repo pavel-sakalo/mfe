9840fc6d495bc45eb767f533801590b912485b60
Pavel Sakalo <pashass4@gmail.com>
2013-02-12 23:28:38 +0200

    dasd

--- source/Types/std_types.h
+++ source/Types/std_types.h
@@ -1,19 +1,24 @@
 #ifdef MFE_TYPES_STDTYPES_H_
 #define MFE_TYPES_STDTYPES_H_
 
-typedef signed char Char;
-typedef unsigned char UChar;
-typedef signed wchar_t WChar;
-typedef unsigned wchar_t UWchar;
-typedef signed short Short;
-typedef unsigned short UShort;
-typedef signed int Int;
-typedef unsigned int UInt;
-typedef signed long Long;
-typedef unsigned long ULong;
-typedef signed long long Int64;
-typedef unsigned long long UInt64;
-typedef float Float;
-typedef double Double;
+namespace MFE
+{
+
+	typedef signed char Char;
+	typedef unsigned char UChar;
+	typedef signed wchar_t WChar;
+	typedef unsigned wchar_t UWchar;
+	typedef signed short Short;
+	typedef unsigned short UShort;
+	typedef signed int Int;
+	typedef unsigned int UInt;
+	typedef signed long Long;
+	typedef unsigned long ULong;
+	typedef signed long long Int64;
+	typedef unsigned long long UInt64;
+	typedef float Float;
+	typedef double Double;
+
+} // namespace MFE
 
 #endif // MFE_TYPES_STDTYPES_H_
