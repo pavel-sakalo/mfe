1b48fd412287a404cc54336c0a0eb37c43481602
Pavel Sakalo <pashass4@gmail.com>
2013-02-15 20:19:08 +0200

    takoe

---
+++ source/core/core.h
@@ -0,0 +1,16 @@
+#ifndef MFE_CORE_CORE_H_
+#define MFE_CORE_CORE_H_
+
+namespace mfe
+{
+
+	class Core 
+	{
+	public:
+		Core();
+		~Core();
+	};
+
+} // namespace mfe
+
+#endif // MFE_CORE_CORE_H_
---
+++ source/events/event.h
@@ -0,0 +1,16 @@
+#ifndef MFE_EVENTS_EVENT_H_
+#define MFE_EVENTS_EVENT_H_
+
+namespace mfe
+{
+
+	class Event
+	{
+	public:
+		Event();
+		~Event();
+	};
+
+} // namespace mfe
+
+#endif // MFE_EVENTS_EVENT_H_
