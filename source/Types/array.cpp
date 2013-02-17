dc5e277d27399a6ac38df80af706b83e1605b18b
Pavel Sakalo <pashass4@gmail.com>
2013-02-17 11:04:59 +0200

    final

--- source/Types/array.cpp
+++ source/Types/array.cpp
@@ -1,50 +1,82 @@
-#include "array.h"
+// Array.cpp: определяет точку входа для консольного приложения.
+//
 
-using namespace mfe;
+#include "stdafx.h"
+#include <stdlib.h>
 
 template <class T> 
-Array<T>::Array() 
-{
-	buffer = (T*)malloc(default_array_size * sizeof(T));
-	mem_allocated = default_array_size * sizeof(T);
-	current_size = 0;
+class Array {
+public:
+	Array(const int len = 10);
+	~Array(); 
+	void PushBack(const T var);
+	int GetSize();
+	Array& operator = (Array& x); 
+	T& operator [] (const int i);
+private:
+	T *arr;
+	int size;
+	int mem;
+};
+
+template<class T> Array<T>::Array(const int len = 100) {
+		arr = new T [len];
+		mem = len;
+		size = 0;
 }
-
-template <class T> 
-Array<T>::Array(const INT len) 
-{
-	buffer = (T*)malloc(len * sizeof(T));
-	mem_allocated = len * sizeof(T);
-	current_size = 0;
+template<class T> Array<T>::~Array() { 	delete [] arr; 	mem = 0, size = 0;	}
+template<class T> int Array<T>::GetSize()  { return size; }
+template<class T> void Array<T>::PushBack(const T var) {
+		size++;
+		if( size > mem) {
+			//перевыделение памяти
+			mem *= 2;
+			T *tmp = new T [mem];
+			memcpy(tmp,arr,(size-1)*sizeof(T));
+			delete [] arr;
+			arr = new T [mem];
+			memcpy(arr,tmp,(size-1)*sizeof(T));
+			delete [] tmp;
+			arr[size-1] = var;
+		}
+		else
+			arr[size-1] = var;
 }
-
-template <class T> 
-Array<T>::~Array() 
-{
-	assert(free(buffer));
-	mem_allocated = 0;
-	current_size = 0;	
-}
-
-template <class T> 
-VOID Array<T>::PushBack(const T value) 
-{
-	current_size++;
-	if (current_size > mem_allocated / sizeof(T)) 
-	{
-		mem_allocated *= 2;
-		buffer = (T*)realloc(buffer, mem_allocated);
-		assert(buffer);
+template<class T> Array<T>& Array<T>::operator = (Array<T>& x) {
+	if( this == &x)
+		return *this;
+	if( size < x.GetSize()) {
+		do {
+			delete [] arr;	
+			mem *= 2;
+			arr = new T [mem];
+		} while( mem < x.GetSize() );
 	}
-	buffer[current_size - 1] = value;
+	memcpy(arr,x.arr,x.GetSize()*sizeof(T));
+	size = x.GetSize();
+	return *this;
+}
+template<class T> T& Array<T>::operator [] (const int i) {
+	if( i >= size) {
+		perror("Out of range");
+	}
+	else 
+		return arr[i];
 }
 
-template <class T> 
-Array<T>& Array<T>::operator = (const Array &value) 
+
+
+int main()
 {
-	size = 0;
-	for(INT i = 0; i < value.current_size; i++) 
-		this.PushBack(value[i]);
-	return this;
+	Array<int> arr;
+	for(int i = 0; i < 20; i++) {
+		arr.PushBack(i);
+	}
+	Array<int> tmp;
+	tmp = arr;
+	for(int i = 0; i < 20; i++) {
+		printf("%d ",tmp[i]);
+	}
+	system("pause");
+	return 0;
 }
-
